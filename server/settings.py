11f2443ca4db55c6039d893163bd0feea644145c
Игорь Перепилицын <kylehydeme@gmail.com>
2017-04-29 16:31:18 +0300

    remove flask_restful

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,4 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SQLALCHEMY_DATABASE_URI = os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.path.join(BASE_DIR, 'fires.sqlite3')
