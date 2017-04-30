e0b805fdd1145820c6d00f555adc24fdd0202af9
Игорь Перепилицын <kylehydeme@gmail.com>
2017-04-30 16:09:59 +0300

    add database uri

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,5 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SQLALCHEMY_DATABASE_URI = os.path.join(BASE_DIR, 'fires.sqlite')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', os.path.join(BASE_DIR, 'fires.sqlite'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
